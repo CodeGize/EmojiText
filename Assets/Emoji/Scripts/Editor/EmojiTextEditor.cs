0e2c2bae4c03695aae510874545131850a67b286
CgChen <ccggzz123@aliyun.com>
2018-05-25 15:31:29 +0800

    工程整理 去除Resource加载方式 修改操作说明 修复多国语言下，某些语言无法正确显示图片的问题

---
+++ Assets/Emoji/Scripts/Editor/EmojiTextEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using TextEditor = UnityEditor.UI.TextEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(EmojiText), true)]
+public class EmojiTextEditor : TextEditor
+{
+    private SerializedProperty m_conten;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        m_conten = serializedObject.FindProperty("emojiContent");
+        var tar = (EmojiText)target;
+        if (tar.canvas != null)
+            tar.canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.TexCoord1;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (m_conten != null)
+            EditorGUILayout.PropertyField(m_conten);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
